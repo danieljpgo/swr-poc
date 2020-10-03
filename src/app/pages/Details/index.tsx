7096f99c1f65777eb941b55fe4ff7cd3fc8cf4c1
Daniel Jorge <djpgonzaga@gmail.com>
2020-10-02 21:10:57 -0300

    feat: add axios and json-server for mocking and fetch requests

--- src/app/pages/Details/index.tsx
+++ src/app/pages/Details/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Container } from './styles';
+import axios from 'axios';
+import useSWR from 'swr';
 
-const Details = () => (
-  <Container>
-    a
-  </Container>
-);
+const fetcher = async (endpoint: string) => {
+  const response = await axios.get(endpoint)
+
+  return response;
+}
+
+const Details = () => {
+  const { data } = useSWR('http://localhost:3000/users/1', fetcher)
+
+  console.log(data);
+
+  return (
+    <Container>
+      a
+    </Container>
+  )
+};
 
 export default Details;
